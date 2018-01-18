678983ce8a521e4a2faa7458803233b077652587
denssk <denssky@yandex.ru>
2018-01-19 02:41:16 +0300

    Update metrics_exporter.go

--- prometheus/metrics_exporter.go
+++ prometheus/metrics_exporter.go
@@ -35,42 +35,13 @@ func RetrieveKafkaConnectMetrics(hostString string) string {
         fmt.Println(err)
       }
 
-      runningTasksCount := 0.0
+      failedTasksCount := 0.0
       for _, task := range status.Tasks {
-        if task.State == "RUNNING" {
-          runningTasksCount += 1.0
+        if task.State == "FAILED" {
+          failedTasksCount += 1.0
         }
       }
-      line := fmt.Sprintf("kafka_connect_runningtaskscount{connector=\"%s\"} %.1f\n", connector, runningTasksCount)
-      output += line
-  }
-
-  return output
-}
-
-func RetrieveSchemaRegistryMetrics(hostString string) string {
-  var output string
-
-  subjects, err := kafka_connect.ListSubjects(hostString, true)
-  if err != nil {
-    fmt.Println(err)
-  }
-
-  subjectCount := len(subjects)
-  output += "# TYPE schema_registry_subjectcount gauge\n"
-  line := fmt.Sprintf("schema_registry_subjectcount %.1f\n", float64(subjectCount))
-  output += line
-
-  output +=  "# TYPE schema_registry_versioncount gauge\n"
-  for _, subject := range subjects {
-      versions, err := kafka_connect.ListVersions(hostString, subject, true)
-
-      if err != nil {
-        fmt.Println(err)
-      }
-
-      versionCount := len(versions)
-      line := fmt.Sprintf("schema_registry_versioncount{subject=\"%s\"} %.1f\n", subject, float64(versionCount))
+      line := fmt.Sprintf("kafka_connect_failed_tasks_count{connector=\"%s\"} %.1f\n", connector, failedTasksCount)
       output += line
   }
 
@@ -99,8 +70,7 @@ func RetrievePrometheusMetrics() {
   for true {
     fmt.Println("Refreshing metrics...")
     kafkaConnectOutput := RetrieveKafkaConnectMetrics(kafkaConnectHostString)
-    schemaRegistryOutput := RetrieveSchemaRegistryMetrics(schemaRegistryHostString)
-    ResponseString = kafkaConnectOutput + schemaRegistryOutput
+    ResponseString = kafkaConnectOutput
     fmt.Println("Metrics refresh complete!")
 
     time.Sleep(time.Duration(metricsRefreshRate) * time.Second)
